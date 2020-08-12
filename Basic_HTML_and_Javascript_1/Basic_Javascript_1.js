084152118c1d2b0bd988f31b698ffcbeba4def96
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-08-12 15:07:49 -0500

    changes

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,10 @@
+alert("Hello, World! I am an alert box!");
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementbyId("Irish").innerHTML =
+		result;
+	}
+	
+
+	
